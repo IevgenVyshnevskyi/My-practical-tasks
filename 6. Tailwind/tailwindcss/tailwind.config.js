1d9e40ac369be96e4367fe16daf35ae7729b3275
Ievgen Vyshnevskyi <evgenii.vishnevsk@gmail.com>
2023-11-05 20:07:13 +0200

    added examples of dark theme

--- 6. Tailwind/tailwindcss/tailwind.config.js
+++ 6. Tailwind/tailwindcss/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: ["./index.html"],
+   //darkMode: 'class',  // when our work is finished we can remove 'darkMode: 'class''
+   theme: {
+      extend: {},
+   },
+   plugins: [],
 }
