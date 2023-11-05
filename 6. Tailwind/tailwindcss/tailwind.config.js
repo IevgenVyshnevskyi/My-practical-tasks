65179ba2d8d2bbbc0a5fc51a062acfea0522abc9
Ievgen Vyshnevskyi <evgenii.vishnevsk@gmail.com>
2023-11-05 21:51:01 +0200

    added examples of customization

--- 6. Tailwind/tailwindcss/tailwind.config.js
+++ 6. Tailwind/tailwindcss/tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
    content: ["./index.html"],
-   //darkMode: 'class',  // when our work is finished we can remove 'darkMode: 'class''
    theme: {
-      extend: {},
+     extend: {
+       gridTemplateRows: {  // де "gridTemplateRows" - cвойство, яке відповвідає за відповівдний клас
+         'layout': 'auto 1fr auto',  // де "'layout': 'auto 1fr auto'" - ім'я по якому ми можемо використовувати суфыкс для класу
+       },
+       colors: {  // де "colors" - добавляємо здатність використання неіснуючого кольору
+         'logo-main': '#9b0404',  // де "'logo-main': '#9b0404'" - неіснуючий колір (як приклад)
+       },
+       fontFamily: {  // де "fontFamily" - добавляємо здатність використання неіснуючої сім'ї стилів шрифтів
+         main: ['Montserrat'],  // де "main: ['Montserrat']" - неіснуюча сім'я стилів шрифтів  (отримана з сайту "https://fonts.google.com/")
+       },
+     },
    },
    plugins: [],
-}
+ }
